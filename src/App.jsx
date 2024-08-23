a756eb6499fc6cf3cfe5d6368f1b953bf30dad9d
Hamzaabro5 <hamzaabro683@gmail.com>
2024-08-23 18:10:46 +0500

    Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = ()=> {
   }
 
   const editTodo = (index)=>{
-    console.log(`todo edit` , index);
+    // console.log(`todo edit` , index);
     const updatedTodo = prompt(`Edit Your Todo`)
     todo.splice(index , 1 , updatedTodo)
     setTodo([...todo])
@@ -30,17 +30,21 @@ const App = ()=> {
 
   return(
   <>
-  <h1>Todo App</h1>
-  <form onSubmit={addTodo}>
-    <input type="text" placeholder="Enter Todo" ref={todoValue}/>
-    <button type="submit">Add Todo</button>
+  <h1 className="text-center my-5">Todo App</h1>
+  <form className="text-center" onSubmit={addTodo}>
+    <div>
+    <input type="text" className="rounded-5 p-2 w-25" placeholder="Enter Todo" ref={todoValue}/>
+    </div>
+    <div>
+    <button className="btn btn-primary rounded-5 my-4 fw-bold" type="submit">Add Todo</button>
+    </div>
   </form>
-  <ul>
+  <ul className="text-center">
     {todo.map((item , index)=>{
       return <div key={index}>
-      <li>{item}</li>
-      <button onClick={()=> deleteTodo(index)}>Delete</button>
-      <button onClick={()=> editTodo(index)}>Edit</button>
+      <h1 className="mt-5">{item}</h1>
+      <button className="btn btn-danger rounded-5" onClick={()=> deleteTodo(index)}>Delete</button>
+      <button className="btn btn-success rounded-5 ms-4" onClick={()=> editTodo(index)}>Edit</button>
       </div>
     })}
   </ul>
